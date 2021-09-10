9a98230108f19b094a9b3fd90a3466fa9eb64c26
pashamakhilkumarreddy <pashamakhilkumarreddy@gmail.com>
2021-09-10 19:00:49 +0530

    add visitor pattern

--- oops/main.go
+++ oops/main.go
@@ -9,17 +9,22 @@ import (
 
 func main() {
 	log.Println("Oops in Golang")
-	expressLine := services.Bus{
-		Name: "Express Line",
-	}
-	expressLine.Passengers = make([]services.Passenger, 0)
-	expressLine.Passengers = append(expressLine.Passengers, services.Passenger{SSN: "001"})
-	expressLine.Passengers = append(expressLine.Passengers, services.Passenger{SSN: "002"})
-	expressLine.Passengers = append(expressLine.Passengers, services.Passenger{SSN: "003"})
+	expressLine := services.NewBus("Express Line")
+	expressLine.Add(services.NewPassenger("001", nil))
+	expressLine.Add(services.NewPassenger("002", nil))
+	expressLine.Add(services.NewPassenger("003", nil))
 
 	ssns := make([]string, 0)
-	for _, p := range expressLine.Passengers {
-		ssns = append(ssns, p.SSN)
-	}
+	expressLine.VisitPassengers(func(p services.Passenger) { ssns = append(ssns, p.SSN) })
 	fmt.Printf("The bus carries %d passengers, here are their SSN's: %v\n", len(ssns), ssns)
+
+	var seatNumber uint8
+	expressLine.UpdatePassenger(func(p *services.Passenger) {
+		seatNumber++
+		p.SeatNumber = seatNumber
+	})
+
+	expressLine.VisitPassengers(func(p services.Passenger) {
+		fmt.Printf("Passenger with SSN %q has seat %d\n", p.SSN, p.SeatNumber)
+	})
 }
--- oops/services/busservice.go
+++ oops/services/busservice.go
@@ -8,6 +8,52 @@ type Passenger struct {
 
 // Bus carriers Passengers from A to B if they have a valid bus ticket
 type Bus struct {
-	Name       string
-	Passengers []Passenger
+	name       string
+	passengers map[string]Passenger
+}
+
+func NewPassenger(SSN string, SeatNumber interface{}) Passenger {
+	p := Passenger{}
+	p.SSN = SSN
+	switch v := SeatNumber.(type) {
+	case int:
+	case uint8:
+		p.SeatNumber = uint8(v)
+	}
+	return p
+}
+
+func NewBus(name string) Bus {
+	b := Bus{}
+	b.name = name
+	return b
+}
+
+func (b *Bus) Add(p Passenger) {
+	if b.passengers == nil {
+		b.passengers = make(map[string]Passenger)
+	}
+	b.passengers[p.SSN] = p
+}
+
+func (b *Bus) VisitPassengers(visitor func(Passenger)) {
+	for _, p := range b.passengers {
+		visitor(p)
+	}
+}
+
+func (b *Bus) FindPassenger(ssn string) Passenger {
+	if v, ok := b.passengers[ssn]; ok {
+		return v
+	}
+	return Passenger{}
+}
+
+func (b *Bus) UpdatePassenger(visitor func(*Passenger)) {
+	ps := make(map[string]Passenger, len(b.passengers))
+	for ssn, p := range b.passengers {
+		visitor(&p)
+		ps[ssn] = p
+	}
+	b.passengers = ps
 }
